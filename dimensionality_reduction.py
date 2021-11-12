96ab69f9bc13164c589b4285f9d136ca485bff03
davidemontella <davidelmontella@gmail.com>
2021-11-12 13:19:10 +0100

    Added regession models, model and feature selection methods

---
+++ dimensionality_reduction.py
@@ -0,0 +1,259 @@
+#PCA
+
+import pandas as pd
+df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+X_train, X_test, y_train, y_test = \
+train_test_split(X, y,
+test_size=0.3, random_state=0)
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+X_test_std = sc.transform(X_test)
+
+import numpy as np
+cov_mat = np.cov(X_train_std.T)
+eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
+print('\nEigenvalues \n%s' % eigen_vals)
+tot = sum(eigen_vals)
+var_exp = [(i / tot) for i in sorted(eigen_vals, reverse=True)]
+cum_var_exp = np.cumsum(var_exp)
+
+import matplotlib.pyplot as plt
+plt.bar(range(1,14), var_exp, alpha=0.5, align='center',
+                      label='individual explained variance')
+plt.step(range(1,14), cum_var_exp, where='mid',
+label='cumulative explained variance')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Principal components')
+plt.legend(loc='best')
+plt.show()
+
+eigen_pairs =[(np.abs(eigen_vals[i]),eigen_vecs[:,i]) for i in range(len(eigen_vals))]
+
+eigen_pairs.sort(reverse=True)
+w= np.hstack((eigen_pairs[0][1][:, np.newaxis],
+eigen_pairs[1][1][:, np.newaxis]))
+
+X_train_std[0].dot(w)
+X_train_pca = X_train_std.dot(w)
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+for l, c, m in zip(np.unique(y_train), colors, markers):
+  plt.scatter(X_train_pca[y_train==l, 0],
+                X_train_pca[y_train==l, 1],
+                      c=c, label=l, marker=m)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.show()
+
+###SCIKIT-LEARN PCA
+from matplotlib.colors import ListedColormap
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+  # setup marker generator and color map
+  markers = ('s', 'x', 'o', '^', 'v')
+  colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+  cmap = ListedColormap(colors[:len(np.unique(y))])
+  # plot the decision surface
+  x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+  x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+  xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+  np.arange(x2_min, x2_max, resolution))
+  Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+  Z = Z.reshape(xx1.shape)
+  plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+  plt.xlim(xx1.min(), xx1.max())
+  plt.ylim(xx2.min(), xx2.max())
+  # plot class samples
+  for idx, cl in enumerate(np.unique(y)):
+    plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                  alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2)
+lr = LogisticRegression()
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+lr.fit(X_train_pca, y_train)
+plot_decision_regions(X_train_pca, y_train, classifier=lr)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend(loc='lower left')
+plt.show()
+
+#To see the variance ratio of the features 
+pca = PCA(n_components=None)
+X_train_pca = pca.fit_transform(X_train_std)
+pca.explained_variance_ratio_
+
+#LINEAR DISCRIMINANT ANALYSIS (LDA)
+np.set_printoptions(precision=4)
+mean_vecs = []
+for label in range(1,4):
+  mean_vecs.append(np.mean(X_train_std[y_train==label], axis=0))
+  print('MV %s: %s\n' %(label, mean_vecs[label-1]))
+
+d = 13 # number of features
+S_W = np.zeros((d, d))
+for label,mv in zip(range(1,4), mean_vecs):
+  class_scatter = np.zeros((d, d))
+
+  for row in X_train[y_train == label]:
+    row, mv = row.reshape(d, 1), mv.reshape(d, 1)
+    class_scatter += (row-mv).dot((row-mv).T)
+
+    S_W += class_scatter
+
+print('Within-class scatter matrix: %sx%s' % (S_W.shape[0], S_W.shape[1]))
+print('Class label distribution: %s' % np.bincount(y_train)[1:])
+d = 13 # number of features
+S_W = np.zeros((d, d))
+for label,mv in zip(range(1, 4), mean_vecs):
+  class_scatter = np.cov(X_train_std[y_train==label].T)
+  S_W += class_scatter
+print('Scaled within-class scatter matrix: %sx%s' % (S_W.shape[0], S_W.shape[1]))
+
+mean_overall = np.mean(X_train_std, axis=0)
+d = 13 # number of features
+S_B = np.zeros((d, d))
+for i,mean_vec in enumerate(mean_vecs):
+  n = X_train[y_train==i+1, :].shape[0]
+  mean_vec = mean_vec.reshape(d, 1)
+  mean_overall = mean_overall.reshape(d, 1)
+S_B += n * (mean_vec - mean_overall).dot((mean_vec - mean_overall).T)
+print('Between-class scatter matrix: %sx%s'% (S_B.shape[0], S_B.shape[1]))
+eigen_vals, eigen_vecs = np.linalg.eig(np.linalg.inv(S_W).dot(S_B))
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:,i]) for i in range(len(eigen_vals))]
+eigen_pairs = sorted(eigen_pairs, key=lambda k: k[0], reverse=True)
+print('Eigenvalues in decreasing order:\n')
+for eigen_val in eigen_pairs:
+  print(eigen_val[0])
+  
+tot = sum(eigen_vals.real)
+discr = [(i / tot) for i in sorted(eigen_vals.real, reverse=True)]
+cum_discr = np.cumsum(discr)
+plt.bar(range(1, 14), discr, alpha=0.5, align='center',
+label='individual "discriminability"')
+plt.step(range(1, 14), cum_discr, where='mid',
+label='cumulative "discriminability"')
+plt.ylabel('"discriminability" ratio')
+plt.xlabel('Linear Discriminants')
+plt.ylim([-0.1, 1.1])
+plt.legend(loc='best')
+plt.show()
+w = np.hstack((eigen_pairs[0][1][:, np.newaxis].real,
+                  eigen_pairs[1][1][:, np.newaxis].real))
+X_train_lda = X_train_std.dot(w)
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+for l, c, m in zip(np.unique(y_train), colors, markers):
+  plt.scatter(X_train_lda[y_train==l, 0]*(-1),
+                  X_train_lda[y_train==l, 1]*(-1),
+                            c=c, label=l, marker=m)
+plt.xlabel('LD 1')
+plt.ylabel('LD 2')
+plt.legend(loc='lower right')
+plt.show()
+
+#LDA FROM SCIKIT-LEARN
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+lda = LDA(n_components=2)
+X_train_lda = lda.fit_transform(X_train_std, y_train)
+
+lr = LogisticRegression()
+lr = lr.fit(X_train_lda, y_train)
+plot_decision_regions(X_train_lda, y_train, classifier=lr)
+plt.xlabel('LD 1')
+plt.ylabel('LD 2')
+plt.legend(loc='lower left')
+plt.show()
+
+
+#KERNEL PCA
+from scipy.spatial.distance import pdist, squareform
+from scipy import exp
+from scipy.linalg import eigh
+import numpy as np
+def rbf_kernel_pca(X, gamma, n_components):
+  """
+  RBF kernel PCA implementation.
+  Parameters
+  ------------
+  X: {NumPy ndarray}, shape = [n_samples, n_features]
+  gamma: float
+  Tuning parameter of the RBF kernel
+  n_components: int
+  Number of principal components to return
+  Returns
+  ------------
+  X_pc: {NumPy ndarray}, shape = [n_samples, k_features]
+  Projected dataset
+  """
+  # Calculate pairwise squared Euclidean distances
+  # in the MxN dimensional dataset.
+  sq_dists = pdist(X, 'sqeuclidean')
+  # Convert pairwise distances into a square matrix.
+  mat_sq_dists = squareform(sq_dists)
+  # Compute the symmetric kernel matrix.
+  K = exp(-gamma * mat_sq_dists)
+  # Center the kernel matrix.
+  N = K.shape[0]
+  one_n = np.ones((N,N)) / N
+  K = K - one_n.dot(K) - K.dot(one_n) + one_n.dot(K).dot(one_n)
+  # Obtaining eigenpairs from the centered kernel matrix
+  # numpy.eigh returns them in sorted order
+  eigvals, eigvecs = eigh(K)
+  # Collect the top k eigenvectors (projected samples)
+  X_pc = np.column_stack((eigvecs[:, -i] for i in range(1, n_components + 1)))
+  return X_pc
+  
+  
+from sklearn.datasets import make_moons
+X, y = make_moons(n_samples=100, random_state=123)
+plt.scatter(X[y==0, 0], X[y==0, 1],
+color='red', marker='^', alpha=0.5)
+plt.scatter(X[y==1, 0], X[y==1, 1],
+color='blue', marker='o', alpha=0.5)
+plt.show()
+
+from sklearn.decomposition import PCA
+scikit_pca = PCA(n_components=2)
+X_spca = scikit_pca.fit_transform(X)
+fig, ax = plt.subplots(nrows=1,ncols=2, figsize=(7,3))
+ax[0].scatter(X_spca[y==0, 0], X_spca[y==0, 1],
+color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_spca[y==1, 0], X_spca[y==1, 1],
+color='blue', marker='o', alpha=0.5)
+ax[1].scatter(X_spca[y==0, 0], np.zeros((50,1))+0.02,
+color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_spca[y==1, 0], np.zeros((50,1))-0.02,
+color='blue', marker='o', alpha=0.5)
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+plt.show()
+
+from matplotlib.ticker import FormatStrFormatter
+X_kpca = rbf_kernel_pca(X, gamma=15, n_components=2)
+fig, ax = plt.subplots(nrows=1,ncols=2, figsize=(7,3))
+ax[0].scatter(X_kpca[y==0, 0], X_kpca[y==0, 1],
+color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_kpca[y==1, 0], X_kpca[y==1, 1],color='blue', marker='o', alpha=0.5)
+ax[1].scatter(X_kpca[y==0, 0], np.zeros((50,1))+0.02,
+color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_kpca[y==1, 0], np.zeros((50,1))-0.02,
+color='blue', marker='o', alpha=0.5)
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+ax[0].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+ax[1].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+plt.show()
